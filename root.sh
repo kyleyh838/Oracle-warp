24c7d83b2e495c3644c725434306eeb667ddb139
kyleyh838 <64256385+kyleyh838@users.noreply.github.com>
2022-06-24 17:15:41 +0800

    Update root.sh

--- root.sh
+++ root.sh
@@ -16,5 +16,5 @@ sudo iptables -P INPUT ACCEPT
 sudo iptables -P FORWARD ACCEPT
 sudo iptables -P OUTPUT ACCEPT
 sudo iptables -F
-sudo apt-get purge netfilter-persistent
+sudo apt-get purge netfilter-persistent -y
 sudo reboot
