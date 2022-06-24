19327704937c7c9a23d2a6cb1039c71c7af51592
kyleyh838 <64256385+kyleyh838@users.noreply.github.com>
2022-06-24 10:37:51 +0800

    Update root.sh

--- root.sh
+++ root.sh
@@ -7,6 +7,8 @@ sudo lsattr /etc/passwd /etc/shadow
 
 read -p "自定义ROOT密码:" mima
 echo root:$mima | sudo chpasswd root
+read -p "自定义SSH端口:" duankou
+sudo sed -i 's/^#\?Port.*/Port $duankou/g' /etc/ssh/sshd_config;
 sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
 sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
 sudo service sshd restart
